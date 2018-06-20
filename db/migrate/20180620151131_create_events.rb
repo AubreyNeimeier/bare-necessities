62e6633f32b34852fa5db9295dcfca974e3b3328
AubreyNeimeier <neimeier.1@osu.edu>
2018-06-20 08:37:26 -0700

    added task model and user_id to tables

--- db/migrate/20180620151131_create_events.rb
+++ db/migrate/20180620151131_create_events.rb
@@ -1,7 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.string :title
+      t.integer :date
+      t.integer :start_time
+      t.integer :end_time
+      t.string :description
+      t.integer :user_id
       t.timestamps
     end
   end

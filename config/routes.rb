8088fa3c60b0c51c8072ab7df09f092e4eb9aa81
AubreyNeimeier <neimeier.1@osu.edu>
2018-06-20 08:12:07 -0700

    created user and event class, along with all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'events#index'
+  resources :tasks
+
+  resources :events do 
+    resources :tasks, only: [:show, :edit]
+  end
+
+  get '/signup' =>'users#new'
+  post '/signup' => 'sessions#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+
+
 end

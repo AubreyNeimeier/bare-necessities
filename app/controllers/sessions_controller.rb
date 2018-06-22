5e05c2d35d4f0dcab9802347d2423aa85e062049
AubreyNeimeier <neimeier.1@osu.edu>
2018-06-22 10:53:04 -0700

    removed extra yeilds in application/layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,19 +5,12 @@ class SessionsController < ApplicationController
     end
 
     def create
-
         user = User.find_by(username: params[:user][:username])
-
-        if user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            @user = user
-            redirect_to user_path(@user)
+        if params[:user][:username]
+            login_with_credentials
         else
-            redirect_to login_path
-        end
-        #binding.pry
-
-
+            login_with_omni
+         end
     end
 
     def destroy
@@ -25,6 +18,21 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
     
+    def login_with_credentials
+        if user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            @user = user
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
+    end
+
+    def login_with_omni
+        user = User.from_omniauth(env["omniauth.auth"])
+        session[:user_id] = user.id
+        redirect_to root_path
+    end
 
 end
 

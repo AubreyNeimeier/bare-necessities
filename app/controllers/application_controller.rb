0ae76ba54ca4b6d63119c6a2984fcffb88006a8d
AubreyNeimeier <neimeier.1@osu.edu>
2018-06-27 16:48:03 -0700

    added month_name method and hard codded some empty divs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?, :require_login, :days_in_month, :starting_div_class
+    helper_method :current_user, :logged_in?, :require_login, :days_in_month, :starting_div_class, :number_of_blank_days, :add_empty_divs, :month_name
 
     def starting_div_class
         date = Date.today - Date.today.day
+    end
+
+    def month_name
+        Date::MONTHNAMES[Date.today.month]
+    end
+
+
+    def number_of_blank_days
+        date = Date.today - Date.today.day
+        date.cwday + 1 
+    end
+
+    def add_empty_divs
+        empty_divs = []
+        number_of_blank_days.times do 
+            empty_divs << "<div></div>"
+        end
+        #binding.pry
+        empty_divs.join
     end
 
     
